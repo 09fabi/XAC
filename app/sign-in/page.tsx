982d31770d6b8a34d0c3c2c329ad30bc7317ef01
09fabi <fquezadacisterna@gmail.com>
2025-11-19 13:19:17 -0300

    clerk

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -4,27 +4,16 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="flex justify-center mt-10">
-      <SignIn
+      <SignIn 
         routing="path"
         path="/sign-in"
         appearance={{
           elements: {
-            formButtonPrimary: "bg-black text-white",
-          },
-        }}
-        signInOptions={{
-          identifier: {
-            emailAddress: true,
-          },
-          strategyOverrides: {
-            email_code: true,
-            email_link: true,
-            password: true,
-            oauth_google: true,
+            formButtonPrimary: "bg-black text-white hover:bg-neutral-800",
+            card: "shadow-xl",
           },
         }}
       />
     </div>
   );
 }
-
--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -4,23 +4,16 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="flex justify-center mt-10">
-      <SignUp
+      <SignUp 
         routing="path"
         path="/sign-up"
         appearance={{
           elements: {
-            formButtonPrimary: "bg-black text-white",
-          },
-        }}
-        signUpOptions={{
-          strategyOverrides: {
-            email_code: true,
-            password: true,
-            oauth_google: true,
+            formButtonPrimary: "bg-black text-white hover:bg-neutral-800",
+            card: "shadow-xl",
           },
         }}
       />
     </div>
   );
 }
-
